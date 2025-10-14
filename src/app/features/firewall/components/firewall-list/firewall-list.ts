2172d3ea40e469d2c6ad8538c1ac64737136c065
benadjal <adame.benadjal@gmail.com>
2025-10-14 21:44:11 +0200

    feat(firewall): display firewalls in p-table

--- src/app/features/firewall/components/firewall-list/firewall-list.ts
+++ src/app/features/firewall/components/firewall-list/firewall-list.ts
@@ -3,10 +3,14 @@ import { FirewallService } from '../../services/firewallService';
 import { AsyncPipe } from '@angular/common';
 import { FirewallForm } from '../firewall-form/firewall-form';
 import { switchMap } from 'rxjs';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ChipModule } from 'primeng/chip';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-firewall-list',
-  imports: [AsyncPipe,FirewallForm],
+  imports: [AsyncPipe,FirewallForm,TableModule,ButtonModule,ChipModule,BadgeModule],
   templateUrl: './firewall-list.html',
   styleUrl: './firewall-list.scss'
 })
