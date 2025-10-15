132ba2653c085e6ac9e93afdd4daabeab7678152
benadjal <adame.benadjal@gmail.com>
2025-10-15 16:30:03 +0200

    feat(ui) side-bar with buttons to navigate through the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { Toolbar } from "./shared/components/toolbar/toolbar";
+import { SideBar } from "./shared/components/side-bar/side-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastModule, Toolbar],
+  imports: [RouterOutlet, ToastModule, Toolbar, SideBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
