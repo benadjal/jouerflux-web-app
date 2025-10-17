ef9d11a1fd849896177b1860b2db96ef281edf34
benadjal <adame.benadjal@gmail.com>
2025-10-17 09:56:15 +0200

    feat(firewall): get all firewalls without pagination

--- src/app/features/firewall/services/firewall-service.ts
+++ src/app/features/firewall/services/firewall-service.ts
@@ -1,22 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Firewall, FirewallDetail, FirewallListResponse } from '../models/firewall.model';
-import { Observable, tap } from 'rxjs';
+import {
+  Firewall,
+  FirewallDetail,
+  FirewallListResponse,
+} from '../models/firewall.model';
+import {
+  forkJoin,
+  map,
+  Observable,
+  of,
+  shareReplay,
+  switchMap,
+} from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { SharedService } from '../../../shared/services/shared-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirewallService {
   http = inject(HttpClient);
+  sharedService = inject(SharedService);
 
-  getAllFirewalls(pageNumber : number): Observable<FirewallListResponse> {
-    return this.http
-      .get<FirewallListResponse>(`${environment.apiURL}/firewalls`,{
-        params : {
-          page : pageNumber
-        }
-      })
+  getAllFirewalls(pageNumber: number): Observable<FirewallListResponse> {
+    return this.http.get<FirewallListResponse>(
+      `${environment.apiURL}/firewalls`,
+      {
+        params: {
+          page: pageNumber,
+        },
+      },
+    );
   }
 
   createFirewall(fireWall: Omit<Firewall, 'id'>): Observable<Firewall> {
@@ -27,12 +42,30 @@ export class FirewallService {
   }
 
   getFirewall(id: number): Observable<FirewallDetail> {
-    return this.http.get<FirewallDetail>(`${environment.apiURL}/firewalls/${id}`).pipe(
-      tap((res) => console.log(res))
-    );
+    return this.http
+      .get<FirewallDetail>(`${environment.apiURL}/firewalls/${id}`)
   }
 
   deleteFirewall(firewallId: number) {
     return this.http.delete(`${environment.apiURL}/firewalls/${firewallId}`);
   }
+
+  allFirewallsWithoutPagination$ = this.sharedService.refreshTrigger$$.pipe(
+    switchMap(() =>
+      this.getAllFirewalls(1).pipe(
+        switchMap((firstPage) => {
+          const totalPages = firstPage.total_pages;
+          const requests = [];
+          for (let i = 2; i <= totalPages; i++) {
+            requests.push(this.getAllFirewalls(i));
+          }
+          return forkJoin([of(firstPage), ...requests]);
+        }),
+        map((responses: FirewallListResponse[]) =>
+          responses.flatMap((res) => res.items),
+        ),
+      ),
+    ),
+    shareReplay(1),
+  )
 }
